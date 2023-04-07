6e79d9bc4d1764eb564937cc500c0b61532c3b57
NougatUser <lasecrafting@gmail.com>
2023-04-07 20:08:05 +0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Создаем папку для сохранения скриншотов
+# dir for screenshot
 mkdir -p out
 
-# Читаем файл ips.txt построчно
+# read ip
 while read line; do
   IP=$(echo "$line" | cut -d':' -f1)
   PORT=$(echo "$line" | cut -d':' -f2)
 
-  # Запускаем vncviewer и делаем скриншот
+  # run vncviewer & screenshot
   (vncviewer -compresslevel 0 -quality 0 -viewonly "$IP:$PORT" & sleep 8; WID=$(xdotool search --name "TightVNC"); if [[ $WID != "" ]]; then import -window "$WID" root "out/${IP}:${PORT}.jpg"; fi) &
-  # Ждем 10 секунд и проверяем статус вывода vncviewer
+  # if it returned an error
   sleep 10
   if ! pgrep vncviewer > /dev/null && [[ ! $(xdotool search --name "TightVNC") ]]; then
     echo "Connection failed for ${IP}:${PORT}"
     pkill vncviewer
   fi
 
-  # Закрываем окно vncviewer
+  # exit
   if [[ $(xdotool search --name "TightVNC") ]]; then
     WID=$(xdotool search --name "TightVNC")
     xdotool windowactivate $WID key "alt+F4"
